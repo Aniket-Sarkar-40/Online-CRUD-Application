29894ca8dfa2f1790997856325fcb00a363cd708
Aniket <aniket10ce@gmail.com>
2022-08-15 17:21:52 +0530

    fixing some more of issues

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 // require("dotenv").config();
-const url = `http://online-crud-application.herokuapp.com/api`;
+const url = `https://online-crud-application.herokuapp.com/api`;
 
 // let secKey = process.env.SECRET_KEY;
 // console.log(secKey);
