d7ebd42e1c744e23cabeef9d4d2feb97de34bdb5
AniketSarkar40 <aniket10ce@gmail.com>
2023-02-16 11:02:07 +0530

    Mongodb connection made

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const DB = `mongodb://Aniket:${process.env.PASSWORD}@ac-k9cy9gl-shard-00-00.ca6nob0.mongodb.net:27017,ac-k9cy9gl-shard-00-01.ca6nob0.mongodb.net:27017,ac-k9cy9gl-shard-00-02.ca6nob0.mongodb.net:27017/?ssl=true&replicaSet=atlas-yu48jq-shard-0&authSource=admin&retryWrites=true&w=majority`
-mongoose.connect(DB,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
+const url = `mongodb+srv://aniket:${process.env.PASSWORD}@cluster0.tospsxr.mongodb.net/user?retryWrites=true&w=majority`;
 
-}).then(()=>{
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("Connection Successful");
-}).catch((error)=>{
+  })
+  .catch((error) => {
     console.log(error);
-})
-
-
+  });
